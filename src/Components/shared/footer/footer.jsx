542e6315f93b3239987e11f11db7b675f5af5dd1
Abanoub Samir <abanoubsamirshenouda@gmail.com>
2021-10-19 18:05:00 +0200

    add footer style

--- src/Components/shared/footer/footer.jsx
+++ src/Components/shared/footer/footer.jsx
@@ -1,6 +1,10 @@
 function FooterComponent() {
   return (
-    <></>
+    <footer className="bg-dark p-5">
+      <div className="container">
+        <p className="fw-bolder text-white">Abanoub Samir@ITI &copy; 2021</p>
+      </div>
+    </footer>
   );
 }
 
