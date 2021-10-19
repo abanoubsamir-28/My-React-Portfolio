aaa1401e0d1eac3d186d4bd8d01ad43d31dee0fc
Abanoub Samir <abanoubsamirshenouda@gmail.com>
2021-10-19 15:57:19 +0200

    configure React Route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 // Packages
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {BrowserRouter as Router, Switch, Route  } from 'react-router-dom';
 // Custom Styling
 import './App.css';
 // Project Components
@@ -9,17 +10,21 @@ import ProductlistComponent from './Components/product/productslist/productlist'
 import ProductComponent from './Components/product/product/product'
 import LoginComponent from './Components/auth/login/login'
 import RegisterComponent from './Components/auth/register/register'
+import HomeComponent from './Components/Home/home';
 
 
 function App() {
   return (
-    <>
-    <NavbarComponent></NavbarComponent>
-    <LoginComponent></LoginComponent>
-    <RegisterComponent></RegisterComponent>
-    <ProductlistComponent></ProductlistComponent>
-    <ProductComponent></ProductComponent>
-    <FooterComponent></FooterComponent>
+    <> 
+      <Router>
+      <NavbarComponent/>
+      <Route path="/" exact component={HomeComponent} />
+      <Route path="/login" component={LoginComponent} />
+      <Route path="/register" component={RegisterComponent} />
+      <Route path="/shop" component={ProductlistComponent} />
+      <Route path="/product/:id" component={ProductComponent} />
+      <FooterComponent/>
+      </Router>
     </>
   );
 }
---
+++ src/Components/Home/home.jsx
@@ -0,0 +1,10 @@
+function HomeComponent() {
+  return (
+    <div className="w-100 h-100">
+      <h1>Home Page</h1>
+      {/* <img src="../../assests/Home.webp" className="w-100" /> */}
+    </div>
+  );
+}
+
+export default HomeComponent;
